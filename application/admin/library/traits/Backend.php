8ca42babd27e2fb6f49b203ec00f4358b3fb8428
王超洪 <510461252@qq.com>
2018-11-04 09:28:23 +0800

    20181104

--- application/admin/library/traits/Backend.php
+++ application/admin/library/traits/Backend.php
@@ -89,7 +89,7 @@ trait Backend
                     if ($this->modelValidate)
                     {
                         $name = str_replace("\\model\\", "\\validate\\", get_class($this->model));
-                        $validate = is_bool($this->modelValidate) ? ($this->modelSceneValidate ? $name . '.add' : true) : $this->modelValidate;
+                        $validate = is_bool($this->modelValidate) ? ($this->modelSceneValidate ? $name . '.add' : $name) : $this->modelValidate;
                         $this->model->validate($validate);
                     }
                     $result = $this->model->allowField(true)->save($params);
@@ -140,8 +140,8 @@ trait Backend
                     //是否采用模型验证
                     if ($this->modelValidate)
                     {
-                        $name = basename(str_replace('\\', '/', get_class($this->model)));
-                        $validate = is_bool($this->modelValidate) ? ($this->modelSceneValidate ? $name . '.edit' : true) : $this->modelValidate;
+                        $name = str_replace("\\model\\", "\\validate\\", get_class($this->model));
+                        $validate = is_bool($this->modelValidate) ? ($this->modelSceneValidate ? $name . '.edit' : $name) : $this->modelValidate;
                         $row->validate($validate);
                     }
                     $result = $row->allowField(true)->save($params);
